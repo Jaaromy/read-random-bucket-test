59f0e65797e2f4ca8a19c7eb1a1c5ff08080080c
Jaaromy Zierse <jaaromy.zierse@observian.com>
2018-06-28 15:11:44 -0600

    Get all without weird fuss.

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const aws = require('aws-sdk');
 const Promise = require('bluebird');
 aws.config.setPromisesDependency(Promise);
 const s3 = new aws.S3();
+const ec2 = new aws.EC2({
+	region: 'us-west-2'
+});
 
 function rand(min, max) {
 	return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -23,16 +26,16 @@ function randomBucket() {
 
 const getAllKeys = async (bucketName) => {
 	// Prefixes are used to fetch data in parallel.
-	const numbers = '0123456789'.split('');
-	const letters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
-	const special = "!-_'.*()".split(''); // "Safe" S3 special chars
-	const prefixes = [...numbers, ...letters, ...special];
+	// const numbers = '0123456789'.split('');
+	// const letters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
+	// const special = "!-_'.*()".split(''); // "Safe" S3 special chars
+	// const prefixes = [...numbers, ...letters, ...special];
+	const prefixes = [''];
 
 	// array of params for each listObjectsV2 call
 	const arrayOfParams = prefixes.map((prefix) => {
 		return {
-			Bucket: bucketName,
-			Prefix: prefix
+			Bucket: bucketName
 		}
 	});
 
@@ -60,3 +63,18 @@ randomBucket()
 	.then(obj => {
 		console.log(JSON.stringify(obj));
 	});
+
+// let p = {
+// 	DryRun: false,
+// 	Filters: [{
+// 		Name: 'instance-id',
+// 		Values: [
+// 			'i-0c9ee8d8b7df97c42'
+// 		]
+// 	}]
+// };
+
+// ec2.describeInstances(p).promise()
+// 	.then(details => {
+// 		console.log(JSON.stringify(details, null, 2));
+// 	})
